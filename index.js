c7b8c0553625e8b34652de2a0fe55748e81db4b1
Ярослав Пруцков <Roket-liger@yandex.ru>
2025-03-21 17:25:46 +0400

    connect to DB

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session')
 const exphbs = require('express-handlebars')
+const MySQLStore = require('express-mysql-session')
 
 const PORT = process.env.PORT || 3000
 
+let sets = {
+    host: 'platon.teyhd.ru',
+    user: 'student',
+    password: 'studpass',
+    database: 'prutskov_todo',
+    port: '3407',
+    charset: 'utf8mb4_general_ci',
+  };
 const app = express()
+const pool = mysql.createPool(sets).promise();
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
@@ -16,15 +27,14 @@ app.set('views','views')
 
 async function start() {
     try {
-        await mongoose.connect('', {
-            useNewUrlParser: true,
-            useFindAndModify: false
-        })
+        const connection = await pool.getConnection();
+        console.log('Успешное подключение к БД');
+        connection.release();
         app.listen(PORT, () => {
             console.log('Server has been started...')
         })
     } catch (e) {
-        console.log(e)
+        console.error('Ошибка подключения к БД:', e)
     }
     
 }
